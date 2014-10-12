a9890dcbfe98410b69743d7ad21189ac0320a948
hammam hammad <hhammad@hammams-MacBook-Pro.local>
2014-10-12 21:25:22 +0300

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by hammam hammad on 10/12/14.
+//  Copyright (c) 2014 hammam hammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by hammam hammad on 10/12/14.
+//  Copyright (c) 2014 hammam hammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
